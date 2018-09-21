54d9af5f2e170d288b9033f6027892fe33c7e732
cwohlbrecht <chris.wohlbrecht@onesty-direct.de>
2018-09-21 08:27:45 +0200

    wip

--- src/main/java/de/onestydirect/openiban/data/loader/services/BankDataServiceImpl.java
+++ src/main/java/de/onestydirect/openiban/data/loader/services/BankDataServiceImpl.java
@@ -3,7 +3,6 @@ package de.onestydirect.openiban.data.loader.services;
 import de.onestydirect.openiban.data.loader.models.BankData;
 import de.onestydirect.openiban.data.loader.models.BankDataRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,7 +11,7 @@ public class BankDataServiceImpl implements BankDataService {
 
 	private BankDataRepository bankDataRepository;
 
-	public BankDataServiceImpl(BankDataRepository bankDataRepository) {
+	public BankDataServiceImpl(final BankDataRepository bankDataRepository) {
 		this.bankDataRepository = bankDataRepository;
 	}
 
