a184da0c5236409c67db05c17cb7a20c5dcba92e
cwohlbrecht <chris.wohlbrecht@onesty-direct.de>
2018-10-04 10:35:41 +0200

    add test, change excel file path

---
+++ src/test/java/de/onestydirect/openiban/data/loader/services/ExcelLoaderServiceImplTest.java
@@ -0,0 +1,11 @@
+package de.onestydirect.openiban.data.loader.services;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+public class ExcelLoaderServiceImplTest {
+
+}
