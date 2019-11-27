1a4d6b3f199b4488fc3f8ce9f83248f5a3c982ba
Chris <Wohlbrecht>
2019-11-27 12:55:28 +0100

    increase java version to 11 and spring boot to 2.2.1.Release

--- dockerfile
+++ dockerfile
@@ -1,8 +1,11 @@
-FROM openjdk:8-jdk
+FROM openjdk:11-jdk
 
 LABEL autor="Chris Wohlbrecht"
 
 
 ARG JAR_FILE=target/loader-0.0.2-SNAPSHOT.jar
 ADD ${JAR_FILE} loader.jar
+
+USER 9000
+
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/loader.jar"]
--- src/test/java/de/onestydirect/openiban/data/loader/LoaderApplicationTests.java
+++ src/test/java/de/onestydirect/openiban/data/loader/LoaderApplicationTests.java
@@ -6,9 +6,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-@DataJpaTest
 public class LoaderApplicationTests {
 
 	@Test
